61a59a30f8b9b0b8428aeeef488838088697b269
Simon Schoelly <sischoel@gmail.com>
2021-04-18 19:57:10 +0200

    Add vertex_labels keyword to WL kernel

--- src/utils.jl
+++ src/utils.jl
@@ -6,3 +6,21 @@ A singleton type to denote that some argument to a function or constructor shoul
 be automatically determined.
 """
 struct Auto end
+
+const LabelsType = Union{Auto, Colon, Tuple{Vararg{Int}}, Tuple{Vararg{Symbol}}}
+
+_labels(::Colon, types) = fieldnames(types)
+
+function _is_suitable_label_type(T)
+
+    return T <: Union{Integer, AbstractString, AbstractChar, Symbol}
+end
+
+function _labels(::Auto, types)
+
+    return filter(i -> _is_suitable_label_type(fieldtype(types, i)), fieldnames(types))
+end
+
+# TODO we could actually verify that the keys are valid
+_labels(keys::Tuple, types) = keys
+
